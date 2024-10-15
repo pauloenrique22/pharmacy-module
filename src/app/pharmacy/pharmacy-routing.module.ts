1a25fbc2aa98f875a24ae46e17150ee4cde20538
Anthony Lijarza <anthonylf797@gmail.com>
2024-10-14 21:25:08 -0400

    Add new prescription Done

--- src/app/pharmacy/pharmacy-routing.module.ts
+++ src/app/pharmacy/pharmacy-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PatientSelectComponent } from './patient-select/patient-select.component';
 import { PrescriptionListComponent } from './prescription-list/prescription-list.component';
 import { PrescriptionDetailComponent } from './prescription-detail/prescription-detail.component';
+//import { AddPrescriptionComponent } from './add-prescription/add-prescription.component'; // importing the add Prescription component
 
 const routes: Routes = [
   { path: '', component: PatientSelectComponent },
   { path: 'prescriptions/:patientId', component: PrescriptionListComponent },
-  { path: 'prescription-detail/:prescriptionId', component: PrescriptionDetailComponent }
+  { path: 'prescription-detail/:prescriptionId', component: PrescriptionDetailComponent },
+  //{ path: 'add-prescription', component: AddPrescriptionComponent} //connecting add-prescription 
 ];
 
 @NgModule({
